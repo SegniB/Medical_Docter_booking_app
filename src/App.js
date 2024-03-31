c8746d93da85d283db601a9f63ac82ccf81dcdb3
SegniB <segni.b2009@gmail.com>
2024-03-31 17:27:08 -0400

    adding login and Sign_up routes to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing_Page from './Components/Landing_Page/Landing_Page.js';
+import Login from './Components/Login/Login.js'
+import Sign_up from './Components/Sign_Up/Sign_Up.js'
 
 import Navbar from './Components/Navbar/Navbar';
 function App() {
@@ -10,7 +12,9 @@ function App() {
         <BrowserRouter>
           <Navbar/>
               <Routes>
-              <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/" element={<Landing_Page />}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/sign_up" element={<Sign_up />}/>
               </Routes>
             
         </BrowserRouter>

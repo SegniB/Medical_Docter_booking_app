086384cd1bf8c163c730a6a13679decbffd2595c
SegniB <segni.b2009@gmail.com>
2024-04-01 19:16:17 -0400

    Adding Notification provider to all components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from './Components/Landing_Page/Landing_Page.js';
 import Login from './Components/Login/Login.js'
 import SignUp from './Components/Sign_Up/Sign_Up.js'
-//import Notification from './Components/Notification/Notification.js'
+import Notification from './Components/Notification/Notification.js'
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation.js'
 
 import Navbar from './Components/Navbar/Navbar';
@@ -12,16 +12,17 @@ function App() {
   return (
     <>
         <BrowserRouter>
-        
+           <Notification>
               <Navbar/>
               <Routes>
               <Route path="/" element={<LandingPage />}/>
               <Route path="/login" element={<Login />}/>
               <Route path="/signup" element={<SignUp />}/>
+              <Route path="/notification" element={<Notification />}/>
              
               <Route path="/instant-consultation" element={<InstantConsultation />} />
               </Routes>
-        
+            </Notification>
         </BrowserRouter>
        
     </>
